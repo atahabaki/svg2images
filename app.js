a029359d106765ba16616d1b57dc20e0d9e151d8
A. Taha Baki <a.taha.baki@outlook.com>
2021-03-07 19:31:31 +0300

    :wastebasket: no need for framerate also...

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ program
 	.option("-i, --input [string]", "Input file, html", "./examples/index.html")
 	.option("-w, --width [int]", "Width of the browser", "1080")
 	.option("-h, --height [int]", "Height of the browser", "1920")
-	.option("-f, --framerate [int]", "Frame rate of the ")
 	.option("-s, --start [int]", "Start frame", "60")
 	.option("-e, --end [int]", "End frame", "60")
 	.option("-o, --output [string]", "Output folder", "./output")
